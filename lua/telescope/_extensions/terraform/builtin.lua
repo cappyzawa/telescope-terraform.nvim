072c97023797ca1a874668aaa6ae0b74425335df
Shu Kutsuzawa <cappyzawa@gmail.com>
2021-12-19 14:11:07 +0900

    enable to scroll preview

--- lua/telescope/_extensions/terraform/builtin.lua
+++ lua/telescope/_extensions/terraform/builtin.lua
@@ -1,9 +1,9 @@
 local conf = require'telescope.config'.values
 local finders = require'telescope.finders'
-local from_entry = require'telescope.from_entry'
 local pickers = require'telescope.pickers'
 local previewers = require'telescope.previewers'
 local utils = require'telescope.utils'
+local putils = require'telescope.previewers.utils'
 
 local terraform_a = require'telescope._extensions.terraform.actions'
 local M = {}
@@ -17,16 +17,30 @@ M.state_list = function(opts)
   local title = 'Terraform States'
   pickers.new(opts, {
     prompt_title = title,
+    layout_config = {
+      horizontal = {
+        preview_cutoff = 100,
+        width = 0.9,
+        height = 0.9,
+      }
+    },
     finder = finders.new_oneshot_job(
       {bin, 'state', 'list'}
     ),
-    previewer = previewers.new_termopen_previewer{
-      get_command = function(entry)
-        local state = from_entry.path(entry)
-        return {bin, 'state', 'show', state}
+    previewer = previewers.new_buffer_previewer{
+      get_buffer_by_name = function (_, entry)
+        return entry.value
       end,
+      define_preview = function (self, entry, status)
+        local command = { bin, 'state', 'show', '-no-color', entry.value }
+        putils.job_maker(command, self.state.bufnr, {
+          value = entry.value,
+          bufname = self.state.bufname,
+          cwd = opts.cwd,
+        })
+      end
     },
-    sorter = conf.file_sorter(opts),
+    sorter = conf.generic_sorter(opts),
     attach_mappings = function(_, map)
 			map('i', '<CR>', nil)
 			map('n', '<CR>', nil)
